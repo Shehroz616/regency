0770a5ba8336704dfd478e25e350ad5239e741c8
Shehorz Saleem <shehrozsaleem616@gmail.com>
2024-08-20 18:58:40 +0500

    detail icons

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -433,10 +433,7 @@ if (list_property_table.length) {
           render: function (data, type, full, meta) {
             return (
               '<div class="d-inline-block">' +
-              '<a href="javascript:;" class="btn btn-sm btn-icon dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="text-primary ti ti-dots-vertical"></i></a>' +
-              '<ul class="dropdown-menu dropdown-menu-end m-0">' +
-              '<li><a href="javascript:;" class="dropdown-item">Details</a></li>' +
-              '</ul>' +
+                '<a href="javascript:;" class="btn btn-sm btn-icon"><i class="text-primary ti ti-file-description"></i></a>'+
               '</div>'
             );
           }
