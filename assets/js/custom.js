09c23228fe1e8d0731a079e815a7a2a6c6457844
Shehroz Saleem <shehrozsaleem616@gmail.com>
2024-08-22 01:42:51 +0500

    update changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -112,6 +112,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 var assigend_property_table = $(".assigend-property-table"),
   list_property_table = $(".list-property-table"),
+  recent_visits_table = $(".recent-visits-table"),
   dt_basic;
 
 // DataTable with buttons
@@ -124,7 +125,9 @@ if (assigend_property_table.length) {
       { data: "" },
       { data: "id" },
       { data: "id" },
-      { data: "full_name" },
+      { data: "property_name" },
+      { data: "lat" },
+      { data: "long" },
     ],
     columnDefs: [
       {
@@ -365,7 +368,7 @@ if (list_property_table.length) {
         { data: '' },
         { data: 'id' },
         { data: 'id' },
-        { data: 'full_name' },
+        { data: 'property_name' },
         { data: 'check_in_time' },
         { data: 'check_out_time' },
         { data: 'com_ment' },
@@ -619,6 +622,127 @@ if (list_property_table.length) {
       }
     });
     $('div.head-label').html('<h4 class="card-title mb-0">Properties List</h4>');
+  }
+if (recent_visits_table.length) {
+    dt_basic = recent_visits_table.DataTable({
+      ajax: assetsPath + 'json/table-datatable2.json',
+      columns: [
+        { data: '' },
+        { data: 'id' },
+        { data: 'id' },
+        { data: 'property_name' },
+        { data: 'check_in_time' },
+        { data: 'check_out_time' },
+        { data: 'com_ment' },
+        { data: '' },
+
+      ],
+      columnDefs: [
+        {
+          // For Responsive
+          className: 'control',
+          orderable: false,
+          searchable: false,
+          responsivePriority: 2,
+          targets: 0,
+          render: function (data, type, full, meta) {
+            return '';
+          }
+        },
+        {
+          // For Checkboxes
+          targets: 1,
+          orderable: true,
+          searchable: false,
+          responsivePriority: 3,
+
+        },
+        {
+          targets: 2,
+          searchable: false,
+          visible: false,
+          orderable: false,
+        },
+        {
+          // Avatar image/badge, Name and post
+          targets: 3,
+          orderable: false,
+          responsivePriority: 4,
+        },
+        {
+          // Avatar image/badge, Name and post
+          targets: 4,
+          orderable: false,
+          responsivePriority: 5,
+        },
+        {
+          // Avatar image/badge, Name and post
+          targets: 5,
+          orderable: false,
+          responsivePriority: 6,
+        },
+        {
+          // Avatar image/badge, Name and post
+          targets: 6,
+          orderable: false,
+          responsivePriority: 7,
+        },
+
+
+        {
+          // Actions
+          targets: -1,
+          title: 'Actions',
+          orderable: false,
+          searchable: false,
+          render: function (data, type, full, meta) {
+            return (
+              '<div class="d-inline-block">' +
+                '<a href="javascript:;" class="btn btn-sm btn-icon"><i class="text-primary ti ti-file-description"></i></a>'+
+              '</div>'
+            );
+          }
+        }
+      ],
+      order: [[2, 'asc']],
+      dom: '<"card-header flex-column flex-md-row"<"head-label text-center"><"dt-action-buttons text-end pt-3 pt-md-0">><"row"<"col-sm-12 col-md-6"><"col-sm-12 col-md-6 d-flex justify-content-center justify-content-md-end"f>>t<"row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+      displayLength: 7,
+      lengthMenu: [7, 10, 25, 50, 75, 100],
+      
+      responsive: {
+        details: {
+          display: $.fn.dataTable.Responsive.display.modal({
+            header: function (row) {
+              var data = row.data();
+              return 'Details of ' + data['full_name'];
+            }
+          }),
+          type: 'column',
+          renderer: function (api, rowIdx, columns) {
+            var data = $.map(columns, function (col, i) {
+              return col.title !== '' // ? Do not show row in modal popup if title is blank (for check box)
+                ? '<tr data-dt-row="' +
+                col.rowIndex +
+                '" data-dt-column="' +
+                col.columnIndex +
+                '">' +
+                '<td>' +
+                col.title +
+                ':' +
+                '</td> ' +
+                '<td>' +
+                col.data +
+                '</td>' +
+                '</tr>'
+                : '';
+            }).join('');
+
+            return data ? $('<table class="table"/><tbody />').append(data) : false;
+          }
+        }
+      }
+    });
+    $('div.head-label').html('<h4 class="card-title mb-0">Recent Visits</h4>');
   }
 
 // Add New record
